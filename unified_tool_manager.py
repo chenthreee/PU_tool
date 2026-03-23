7ea33a61cd82dda96c8c62b1b1021a5ef600e814
chenthreee <1160020938@qq.com>
2026-03-23 14:05:20 +0800

    更新64952表格更新

--- unified_tool_manager.py
+++ unified_tool_manager.py
@@ -53,8 +53,8 @@ except ImportError as e:
 create_can_embedded = None
 create_uart_embedded = None
 
-version = "V1.0.20"
-date = "2026-03-04"
+version = "V1.0.22"
+date = "2026-03-23"
 
 class UnifiedToolManager:
     """统一工具管理器"""
