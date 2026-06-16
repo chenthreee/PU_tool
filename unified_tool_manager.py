f5f801eb6b63618adbfb914fcf92721915e7a2c9
chenthreee <1160020938@qq.com>
2026-06-16 14:41:17 +0800

    解决上位机Can部分存在错位的问题

--- unified_tool_manager.py
+++ unified_tool_manager.py
@@ -53,8 +53,8 @@ except ImportError as e:
 create_can_embedded = None
 create_uart_embedded = None
 
-version = "V1.0.26"
-date = "2026-05-18"
+version = "V1.0.27"
+date = "2026-06-16"
 
 class UnifiedToolManager:
     """统一工具管理器"""
